c6bed8fee96542ec8392ab033fe1e68deab9fead
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-05-15 07:38:22 +0200

    Add files

---
+++ scripts.js
@@ -0,0 +1,26 @@
+ 
+/* CIRCLE & RECTANGLE */
+
+$('#circle-rectangle').on('click', function(event){
+  $('#circle-rectangle').css({
+    '-ms-transform': 'rotate(360deg)',
+     '-moz-transform': 'rotate(360deg)',
+     '-webkit-transform': 'rotate(360deg)',
+     '-o-transform': 'rotate(360deg)',
+     'transform': 'rotate(360deg)'
+  });
+});
+
+/* HAMBURGER-MENU */
+
+$('#menu').on('click', function(event){
+  $('#ul').css({
+    'display': 'block'
+  });
+});
+
+$('#iks').on('click', function(event){
+  $('#ul').css({
+    'display': 'none'
+  });
+});
